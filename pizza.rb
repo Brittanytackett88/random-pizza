e3b6ef1f3fc2be57804e7e383cd978f3dc0761db
brittany <brittanyntackett88@gmail.com>
2017-06-28 10:33:11 -0400

    randomly picking pizza toppings

---
+++ pizza.rb
@@ -0,0 +1,28 @@
+def pepperoni_pizza(num)
+	var = rand(num)
+	if var==2
+		puts "mushrooms"
+	elsif 
+		var==4
+
+		puts "green peppers"
+	elsif
+		var==5
+		puts "sausage"
+		elsif 
+			var==6
+			puts "ham"
+		elsif
+			var==7
+			puts "cheese"
+		elsif 
+			var==10
+			puts "pineapple"
+		else
+			
+
+		puts var
+
+	end
+end
+pepperoni_pizza(15)
